9fda060793e3a27143024fb1d180748680841276
morteza <mortezakhoramdeldreadful@gmail.com>
2023-07-06 17:10:03 +0330

    mpi interfaces

--- slave.py
+++ slave.py
@@ -7,11 +7,14 @@ rank = comm.Get_rank()
 master_hostname = MPI.Get_processor_name()
 master_rank = 0
 
+# Create a shared MPI communicator
+master_comm = comm.Split(color=(rank == master_rank))
+
 # Receive data from master process
-data = comm.recv(source=master_rank, tag=1)
+data = master_comm.recv(source=master_rank, tag=1)
 
 # Perform some task here
 result = rank * 2  # Example task: Multiply rank by 2
 
 # Send result to master process
-comm.send(result, dest=master_rank, tag=2)
+master_comm.send(result, dest=master_rank, tag=2)
